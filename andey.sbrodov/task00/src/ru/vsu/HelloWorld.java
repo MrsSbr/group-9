e77d8d75ba88b1210401da2bc850579a0f08d605
andrey.sbrodov <andrey.sbrodov@team.wrike.com>
2022-09-06 00:34:44 +0300

    HelloWorld2

--- andey.sbrodov/task00/src/ru/vsu/HelloWorld.java
+++ andey.sbrodov/task00/src/ru/vsu/HelloWorld.java
@@ -6,5 +6,6 @@ package ru.vsu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
