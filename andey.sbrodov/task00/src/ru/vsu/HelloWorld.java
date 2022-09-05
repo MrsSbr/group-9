f617867fd9881220bf316ded1da60c3d3edf492a
andrey.sbrodov <andrey.sbrodov@team.wrike.com>
2022-09-06 00:32:36 +0300

    HelloWorld

---
+++ andey.sbrodov/task00/src/ru/vsu/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.vsu;
+
+/**
+ * @author andrey.sbrodov
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
