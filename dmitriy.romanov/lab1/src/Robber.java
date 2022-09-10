16177dc4834ea0445000908be1d23e7c40782ce7
Dmitrii <93659488+dsromanov@users.noreply.github.com>
2022-09-10 13:25:52 +0300

    added lab0

---
+++ dmitriy.romanov/lab1/src/Robber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Robber {
+    public static int robber(int[] nums) {
+        int size = nums.length;
+        if (size == 1) return nums[0];
+        return Math.max(robberRange(nums, 0, size - 2),
+                robberRange(nums, 1, size - 1));
+    }
+
+    static int robberRange(int[] nums, int start, int end) {
+        int d1 = 0, d2 = 0;
+        int d = 0;
+        for (int i = end; i >= start; i--) {
+            d = Math.max(d1, nums[i] + d2);
+            d2 = d1;
+            d1 = d;
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Max sum of robbed money: " + robber(arr));
+    }
+}
