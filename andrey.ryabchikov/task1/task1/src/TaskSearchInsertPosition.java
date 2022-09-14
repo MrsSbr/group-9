b3a2af48c542b82f2774b0fd87f12c33cb54dfd8
andreyRyabchikov <45429367+andreyRyabchikov@users.noreply.github.com>
2022-09-14 16:16:14 +0300

    task1

--- andrey.ryabchikov/task1/task1/src/TaskSearchInsertPosition.java
+++ andrey.ryabchikov/task1/task1/src/TaskSearchInsertPosition.java
@@ -1,45 +1,66 @@
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskSearchInsertPosition {
+
     static int[] readSortedArr() {
+
         Scanner in = new Scanner(System.in);
         System.out.print("nums = ");
         String str = in.nextLine();
         String[] strArr = str.split(" ");
         int numArr[] = new int[strArr.length];
+
         for (int i = 0; i < strArr.length; i++) {
-            numArr[i]=0;
+            numArr[i] = 0;
             try {
-                numArr[i]=Integer.parseInt(strArr[i]);
+                numArr[i] = Integer.parseInt(strArr[i]);
             } catch (final NumberFormatException e) {
                 System.out.printf("error on the element, its value is equal to 0 %s\n",strArr[i]);
+                e.printStackTrace();
             }
-
         }
-        int i=1;
-        while (i<numArr.length && numArr[i]>=numArr[i-1]) {i++;}
-        if(i==numArr.length)
+
+        int i = 1;
+
+        while (i < numArr.length && numArr[i] >= numArr[i - 1]) {
+            i++;
+        }
+
+        if(i == numArr.length) {
             return numArr;
+        }
+
         System.out.printf("Error! Array is not sorted\n");
         return readSortedArr();
+
     }
 
     static int readTarget() {
+
         Scanner in = new Scanner(System.in);
         System.out.print("target = ");
         int num;
+
         try {
             num = in.nextInt();
         } catch (final InputMismatchException e) {
+            e.printStackTrace();
             System.out.printf("error on the element\n");
             num = readTarget();
         }
+
         return num;
+
     }
 
     static int searchInsertPosition(int[] arr, int target) {
-        int left = 0, right = arr.length - 1, midle;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int midle;
+
         while (left <= right) {
             midle = (left + right) / 2;
             if (arr[midle] < target) {
@@ -50,6 +71,8 @@ public class TaskSearchInsertPosition {
                 return midle;
             }
         }
+
         return left;
+
     }
 }
