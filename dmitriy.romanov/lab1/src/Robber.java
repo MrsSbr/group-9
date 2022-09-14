a20de0c14977f3420ae59b74153e909627a5cb8d
Stepanova Diana <stepanova.diana26092002@gmail.com>
2022-09-14 08:51:27 +0300

    changed lab1

--- dmitriy.romanov/lab1/src/Robber.java
+++ dmitriy.romanov/lab1/src/Robber.java
@@ -3,30 +3,42 @@ import java.util.Scanner;
 public class Robber {
     public static int robber(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
+        if (n == 1) {
+            return nums[0];
+        }
         return Math.max(robberRange(nums, 0, n - 2),
-                robberRange(nums, 1,  n - 1));
+                robberRange(nums, 1, n - 1));
     }
 
     static int robberRange(int[] nums, int start, int end) {
-        int d1 = 0, d2 = 0;
-        int d = 0;
+        int house1 = 0, house2 = 0;
+        int houseSum = 0;
         for (int i = end; i >= start; i--) {
-            d = Math.max(d1, nums[i] + d2);
-            d2 = d1;
-            d1 = d;
+            houseSum = Math.max(house1, nums[i] + house2);
+            house2 = house1;
+            house1 = houseSum;
         }
         return d;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int validNum;
         System.out.println("Enter array size: ");
-        int size = input.nextInt();
-        int[] arr = new int[size];
+        if ((validNum = input.nextInt()) < 101 && validNum > 0) {
+            int[] arr = new int[size];
+        } else {
+            System.out.println("Input error");
+        }
+
+
         System.out.println("Enter array elements:");
         for (int i = 0; i < size; i++) {
-            arr[i] = input.nextInt();
+            if ((validNum = input.nextInt()) < 101 && validNum > 0) {
+                arr[i] = input.nextInt();
+            } else {
+                System.out.println("Input error");
+            }
         }
 
         System.out.println("Max sum of robbed money: " + robber(arr));
