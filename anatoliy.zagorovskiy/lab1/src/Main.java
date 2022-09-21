8d93898e6aa42cf234b89b58e88b5aecd08d5eaa
termixxx <zagorovski02@mail.ru>
2022-09-21 08:34:16 +0300

    add lab1 of anatoliy zagorovskiy final

--- anatoliy.zagorovskiy/lab1/src/Main.java
+++ anatoliy.zagorovskiy/lab1/src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-
-        System.out.println(romanToInt(str));
+        System.out.println("Введите от 1 до 15 римских цифр");
+        if (str.length() < 1 || str.length() > 15) {
+            System.out.println("Неправильный ввод");
+        } else {
+            System.out.println(romanToInt(str));
+        }
     }
 
     public static int romanToInt(String str) {
@@ -14,27 +18,13 @@ public class Main {
         int len = str.length();
         for (int i = len - 1; i >= 0; i--) {
             switch (str.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
+                case 'I' -> num = 1;
+                case 'V' -> num = 5;
+                case 'X' -> num = 10;
+                case 'L' -> num = 50;
+                case 'C' -> num = 100;
+                case 'D' -> num = 500;
+                case 'M' -> num = 1000;
             }
             if (num * 3 < res) {
                 res -= num;
