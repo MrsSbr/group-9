e92431d0bce700fc52ee225c62572e7e7e00ff6e
MrsSbr <daria.sanna@gmail.com>
2022-09-21 09:20:25 +0300

    fixed directory

--- ivan.zhadobin/lab1/src/IntegerToRoman.java
+++ ivan.zhadobin/lab1/src/IntegerToRoman.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class IntegerToRoman_ {
+class IntegerToRoman {
     static int[] ch = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     static String[] roman = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
