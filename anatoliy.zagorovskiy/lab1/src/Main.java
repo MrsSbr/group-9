b3196ee7f48b097c5ddea9a0acf2564d16233b1a
termixxx <zagorovski02@mail.ru>
2022-09-22 16:11:59 +0300

    add lab1 of anatoliy zagorovskiy supermegafinal 1.0

--- anatoliy.zagorovskiy/lab1/src/Main.java
+++ anatoliy.zagorovskiy/lab1/src/Main.java
@@ -5,11 +5,33 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите от 1 до 15 римских цифр");
         String str = in.nextLine();
-        if (str.length() < 1 || str.length() > 15) {
+        if (!checkStr(str)) {
             System.out.println("Неправильный ввод");
         } else {
             System.out.println("Результат " + romanToInt(str));
         }
+    }
+
+    public static boolean checkStr(String str) {
+        if (str.length() == 1) {
+            return true;
+        }
+        int cntRimNumbers = 0;
+        for (int i = str.length() - 1; i > 0; i--) {
+            if ((str.charAt(i) == 'V' || str.charAt(i) == 'X') && str.charAt(i - 1) == 'I') {
+                cntRimNumbers++;
+                i--;
+            } else if ((str.charAt(i) == 'L' || str.charAt(i) == 'C') && str.charAt(i - 1) == 'X') {
+                cntRimNumbers++;
+                i--;
+            } else if ((str.charAt(i) == 'D' || str.charAt(i) == 'M') && str.charAt(i - 1) == 'C') {
+                cntRimNumbers++;
+                i--;
+            } else {
+                cntRimNumbers++;
+            }
+        }
+        return cntRimNumbers < 15;
     }
 
     public static int romanToInt(String str) {
@@ -26,11 +48,19 @@ public class Main {
                 case 'D' -> num = 500;
                 case 'M' -> num = 1000;
             }
-            if (num * 3 < res) {
-                res -= num;
-            } else {
-                res += num;
+            if (i != 0) {
+                if ((str.charAt(i) == 'V' || str.charAt(i) == 'X') && str.charAt(i - 1) == 'I') {
+                    res -= 1;
+                    i--;
+                } else if ((str.charAt(i) == 'L' || str.charAt(i) == 'C') && str.charAt(i - 1) == 'X') {
+                    res -= 10;
+                    i--;
+                } else if ((str.charAt(i) == 'D' || str.charAt(i) == 'M') && str.charAt(i - 1) == 'C') {
+                    res -= 100;
+                    i--;
+                }
             }
+            res += num;
         }
         return res;
     }
