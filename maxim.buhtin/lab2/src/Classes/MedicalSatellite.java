f0c62a8d5b492ebfe804a9c517b21a86a46bbbd3
Maxim Buhtin <Maxney2016@yandex.ru>
2022-10-04 23:56:33 +0300

    Спутники

--- maxim.buhtin/lab2/src/Classes/MedicalSatellite.java
+++ maxim.buhtin/lab2/src/Classes/MedicalSatellite.java
@@ -11,23 +11,24 @@ public class MedicalSatellite extends Satellite implements Satellitable {
     protected final int countOfLiveKill;
 
     public MedicalSatellite(int speed, String planet, int life, String name, String appointment,
-                            int countOfLiveSaved,int countOfLiveKill){
-        super(speed,planet,life,name);
-        this.appointment=appointment;
-        this.countOfLiveSaved=countOfLiveSaved;
-        this.countOfLiveKill=countOfLiveKill;
+                            int countOfLiveSaved, int countOfLiveKill) {
+        super(speed, planet, life, name);
+        this.appointment = appointment;
+        this.countOfLiveSaved = countOfLiveSaved;
+        this.countOfLiveKill = countOfLiveKill;
     }
+
     @Override
-    public void show(){
+    public void show() {
         System.out.println("Вы запустили медицинский спутник!");
     }
 
     @Override
     public String toString() {
         return super.toString() + "Имя спутника: " + this.name + "\nСтандартная скорость: " + this.speed +
-                "\nКоличество жизней: " + this.life+ "\nПривязанность к планете: "+ this.planet+
-                "\nНазначение спутника: " + appointment+"\n: "+"\nКоличество убитых пациентов: "+countOfLiveKill+
-        "\nКоличество спасенных пациентов: "+countOfLiveSaved;
+                "\nКоличество жизней: " + this.life + "\nПривязанность к планете: " + this.planet +
+                "\nНазначение спутника: " + appointment + "\n: " + "\nКоличество убитых пациентов: " + countOfLiveKill +
+                "\nКоличество спасенных пациентов: " + countOfLiveSaved;
     }
 
     @Override
@@ -38,14 +39,14 @@ public class MedicalSatellite extends Satellite implements Satellitable {
         return this.name.equalsIgnoreCase(((MedicalSatellite) obj).name)
                 && this.planet == ((MedicalSatellite) obj).planet
                 && this.appointment == ((MedicalSatellite) obj).appointment
-                && this.life==((MedicalSatellite) obj).life
-                && this.countOfLiveKill==((MedicalSatellite) obj).countOfLiveKill
-                && this.countOfLiveSaved==((MedicalSatellite) obj).countOfLiveSaved;
+                && this.life == ((MedicalSatellite) obj).life
+                && this.countOfLiveKill == ((MedicalSatellite) obj).countOfLiveKill
+                && this.countOfLiveSaved == ((MedicalSatellite) obj).countOfLiveSaved;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, planet, appointment, life,countOfLiveKill,countOfLiveSaved);
+        return Objects.hash(name, planet, appointment, life, countOfLiveKill, countOfLiveSaved);
 
     }
 }
--- maxim.buhtin/lab2/src/Main.java
+++ maxim.buhtin/lab2/src/Main.java
@@ -8,21 +8,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Satellite medical1=new MedicalSatellite(60,"Земля",100,"Здоровье+","Вылечить планету",100,1000);
-        Satellite medical2=new MedicalSatellite(80,"Марс",1,"Атлас+","Вылечить того кто лечит планету",0,1);
-        Satellite mart1=new MaritimeSatellite(200,"planet1",100,"Сокол1","Убить всех на свете в воде",100,2);
-        Satellite mart2=new MaritimeSatellite(300,"planet2",120,"Сокол2+","Убить того кто хочет убить всех на свете в воде",20,1);
-        Satellite military1=new MilitarySatellite(400,"Марс",100,"Имя1","Убить всех своих",1);
-        Satellite military2=new MilitarySatellite(500,"Юпитер",150,"Имя2","Убить того кто хочет убить всех своих",1);
+        Satellite medical1 = new MedicalSatellite(60, "Земля", 100, "Здоровье+", "Вылечить планету", 100, 1000);
+        Satellite medical2 = new MedicalSatellite(80, "Марс", 1, "Атлас+", "Вылечить того кто лечит планету", 0, 1);
+        Satellite mart1 = new MaritimeSatellite(200, "planet1", 100, "Сокол1", "Убить всех на свете в воде", 100, 2);
+        Satellite mart2 = new MaritimeSatellite(300, "planet2", 120, "Сокол2+", "Убить того кто хочет убить всех на свете в воде", 20, 1);
+        Satellite military1 = new MilitarySatellite(400, "Марс", 100, "Имя1", "Убить всех своих", 1);
+        Satellite military2 = new MilitarySatellite(500, "Юпитер", 150, "Имя2", "Убить того кто хочет убить всех своих", 1);
 
-        List<Satellite> list=new ArrayList<>();
+        List<Satellite> list = new ArrayList<>();
         list.add(medical1);
         list.add(medical2);
         list.add(mart1);
         list.add(mart2);
         list.add(military1);
         list.add(military2);
-        for(Satellite s:list){
+        for (Satellite s : list) {
             s.show();
             System.out.println();
         }
