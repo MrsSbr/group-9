838398edb96a76719e607dacec6d5a869e5bbba9
Alexander Varfolomeev <alexander.varfolomei@gmail.com>
2022-10-04 07:56:11 +0300

    Fix arrays, naming, work with LocalDate

--- alexander.varfolomeev/lab3/Taxi/src/Models/TaxiDriver.java
+++ alexander.varfolomeev/lab3/Taxi/src/Models/TaxiDriver.java
@@ -2,13 +2,12 @@ package Models;
 
 public class TaxiDriver {
 
+    public String name;
+    public Car car;
     public TaxiDriver(String name, Car car) {
         this.name = name;
         this.car = car;
     }
-
-    public String name;
-    public Car car;
 
     public double getFuelConsumption() {
         return (Math.random() * (car.maxFuelConsumption - car.minFuelConsumption)) + car.minFuelConsumption;
