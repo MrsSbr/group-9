4fc4c3d74b1bc66e52871ea7333940302e92c197
79204 <rozhkov-aleksander.rozhkov@yandex.ru>
2022-10-12 10:08:55 +0300

    add instanseof in main and some fixed

--- alexander.rozhkov/lab2/src/Factory/ConstructionVehiclesFactory.java
+++ alexander.rozhkov/lab2/src/Factory/ConstructionVehiclesFactory.java
@@ -9,13 +9,13 @@ import Service.ConsoleWork;
 
 public class ConstructionVehiclesFactory {
     public ConstructionVehicle createConstructionVehicle(ConstructionVehiclesType type){
-        ConstructionVehicle factory = null;
+        ConstructionVehicle constructionVehicle = null;
         switch (type) {
-            case TRUCK_MIXER -> factory = createTruckMixer();
-            case EXCAVATOR -> factory = createExcavator();
-            case ASPHALT_PAVER -> factory = createAsphaltPaver();
+            case TRUCK_MIXER -> constructionVehicle = createTruckMixer();
+            case EXCAVATOR -> constructionVehicle = createExcavator();
+            case ASPHALT_PAVER -> constructionVehicle = createAsphaltPaver();
         }
-        return factory;
+        return constructionVehicle;
     }
 
     private ConstructionVehicle createAsphaltPaver() {
