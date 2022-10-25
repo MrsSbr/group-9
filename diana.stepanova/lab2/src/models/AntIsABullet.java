c28b1086a7103de6a45fac56155bdad12d14f753
Stepanova Diana <stepanova.diana26092002@gmail.com>
2022-10-25 20:06:50 +0300

    add fixed laba2

--- diana.stepanova/lab2/src/models/AntIsABullet.java
+++ diana.stepanova/lab2/src/models/AntIsABullet.java
@@ -9,9 +9,7 @@ public class AntIsABullet extends Ants {
     public boolean presenceOfPoison;
 
     public AntIsABullet() {
-    }
-
-    ;
+    };
 
     public AntIsABullet( int size, String color, String type, String habitat, boolean presenceOfPoison) {
         super( size, color, type, habitat);
@@ -27,6 +25,34 @@ public class AntIsABullet extends Ants {
 
     public void setPresenceOfPoison(boolean presenceOfPoison) {
         this.presenceOfPoison = presenceOfPoison;
+    }
+    @Override
+    public void live() {
+        if (presenceOfPoison) {
+            System.out.println("Вы неправильно ввели! Я живу в Центральной и Южной Америке!");
+        } else {
+            System.out.println("Я живу в" + getHabitat());
+        }
+    }
+
+    public Ants createAnts() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер муравья: ");
+        int size = Helper.readFromConsole();
+        setSize(size);
+        System.out.print("\nВведите цвет муравьев: ");
+        String color = in.nextLine();
+        setColor(color);
+        System.out.print("\nВведите тип муравьев(самка, самец, рабочий): ");
+        String typeAnts = in.nextLine();
+        setTypeAnts(typeAnts);
+        System.out.print("\nВведите место обитание муравьев: ");
+        String habitat = in.nextLine();
+        setHabitat(habitat);
+        System.out.print("\nВведите наличие яда у муравьев(1-да, 0-нет): ");
+        boolean presenceOfPoison = Helper.intToBoolean(in.nextInt());
+        setPresenceOfPoison(presenceOfPoison);
+        return new AntIsABullet();
     }
 
     @Override
@@ -65,36 +91,4 @@ public class AntIsABullet extends Ants {
                 "; Название->Муравей пуля " ;
 
     }
-
-    @Override
-    public void live() {
-        if (presenceOfPoison) {
-            System.out.println("Вы неправильно ввели! Я живу в Центральной и Южной Америке!");
-        } else {
-            System.out.println("Я живу в" + getHabitat());
-        }
-    }
-
-    @Override
-    public Ants createAnts() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер муравья: ");
-        int size = Helper.readFromConsole();
-        setSize(size);
-        System.out.print("\nВведите цвет муравьев: ");
-        String color = in.nextLine();
-        setColor(color);
-        System.out.print("\nВведите тип муравьев(самка, самец, рабочий): ");
-        String typeAnts = in.nextLine();
-        setTypeAnts(typeAnts);
-        System.out.print("\nВведите место обитание муравьев: ");
-        String habitat = in.nextLine();
-        setHabitat(habitat);
-        System.out.print("\nВведите наличие яда у муравьев(1-да, 0-нет): ");
-        boolean presenceOfPoison = Helper.intToBoolean(in.nextInt());
-        setPresenceOfPoison(presenceOfPoison);
-        return this;
-    }
-
-
 }
