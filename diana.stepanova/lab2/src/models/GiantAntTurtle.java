a849f97e483c85a5e63d3a723215ddd2df1a883c
Stepanova Diana <stepanova.diana26092002@gmail.com>
2022-10-25 20:10:57 +0300

    add fixed laba2

--- diana.stepanova/lab2/src/models/GiantAntTurtle.java
+++ diana.stepanova/lab2/src/models/GiantAntTurtle.java
@@ -18,11 +18,35 @@ public class GiantAntTurtle extends Ants {
     }
 
     public GiantAntTurtle() {
-
-
+    };
+    @Override
+    public void collectFood() {
+        if (typeAnts.equals("рабочий")) {
+            System.out.println("Я собрал еду,спасибо!");
+        } else {
+            System.out.println("Я не могу собрать еду, я не рабочий!");
+        }
+    }
+    public Ants createAnts() {
+        Scanner in = new Scanner((System.in));
+        System.out.print("Введите размер муравья: ");
+        int size = Helper.readFromConsole();
+        setSize(size);
+        System.out.print("\nВведите цвет муравьев: ");
+        String color = in.nextLine();
+        setColor(color);
+        System.out.print("\nВведите тип муравьев(самка, самец, рабочий): ");
+        String typeAnts = in.nextLine();
+        setTypeAnts(typeAnts);
+        System.out.print("\nВведите место обитание муравьев: ");
+        String habitat = in.nextLine();
+        setHabitat(habitat);
+        System.out.print("\nВведите количество шипов у муравьев: ");
+        int spikes = Helper.readFromConsole();
+        setSpikes(spikes);
+        return new GiantAntTurtle();
     }
 
-    ;
 
     public int getSpikes() {
         return spikes;
@@ -70,36 +94,6 @@ public class GiantAntTurtle extends Ants {
 
     }
 
-    @Override
-    public void collectFood() {
-        if (typeAnts == "рабочий") {
-            System.out.println("Я собрал еду,спасибо!");
-        } else {
-            System.out.println("Я не могу собрать еду, я не рабочий!");
-        }
-    }
-
-    @Override
-    public Ants createAnts() {
-        Scanner in = new Scanner((System.in));
-        System.out.print("Введите размер муравья: ");
-        int size = Helper.readFromConsole();
-        setSize(size);
-        System.out.print("\nВведите цвет муравьев: ");
-        String color = in.nextLine();
-        setColor(color);
-        System.out.print("\nВведите тип муравьев(самка, самец, рабочий): ");
-        String typeAnts = in.nextLine();
-        setTypeAnts(typeAnts);
-        System.out.print("\nВведите место обитание муравьев: ");
-        String habitat = in.nextLine();
-        setHabitat(habitat);
-        System.out.print("\nВведите количество шипов у муравьев: ");
-        int spikes = Helper.readFromConsole();
-        setSpikes(spikes);
-
-        return this;
-    }
 
 }
 
