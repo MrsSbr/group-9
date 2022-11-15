314d1775ca7b0e804a0fb8686c32b62ed62b8b5d
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2022-11-15 21:35:57 +0300

    Update SmartHome.java

--- lisa.kolesnikova/lab2/lab2/src/Factory/Interfaces/SmartHome.java
+++ lisa.kolesnikova/lab2/lab2/src/Factory/Interfaces/SmartHome.java
@@ -1,28 +1,9 @@
 package Factory.Interfaces;
 
-import Factory.Models.SmartHomeHeater;
-import Factory.Models.SmartHomeLightning;
 import Models.SmartObject.SmartObject;
 import Enum.*;
 
 
 public interface SmartHome {
     SmartObject createSmartObject(Rooms pos);
-
-    static SmartHome createSmartObjectType(Type type) throws Exception {
-        return switch (type) {
-            case LIGHT -> new SmartHomeLightning();
-            case HEAT -> new SmartHomeHeater();
-            // ...
-            default -> throw new ClassNotFoundException("Класс переданного типа не существует.");
-        };
-    }
-    static SmartObject smartHomeDevices(int n, Rooms room) throws Exception {
-        SmartHome smartHome = switch (n) {
-            case 1 -> SmartHome.createSmartObjectType(Type.LIGHT);
-            case 2 -> SmartHome.createSmartObjectType(Type.HEAT);
-            default -> throw new ClassNotFoundException();
-        };
-        return smartHome.createSmartObject(room);
-    }
 }
