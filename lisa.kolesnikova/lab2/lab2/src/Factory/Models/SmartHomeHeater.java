f3d8dd91085676f09f163b2aa76fc9be80fb7e49
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2022-11-15 21:35:22 +0300

    Update SmartHomeHeater.java

--- lisa.kolesnikova/lab2/lab2/src/Factory/Models/SmartHomeHeater.java
+++ lisa.kolesnikova/lab2/lab2/src/Factory/Models/SmartHomeHeater.java
@@ -1,8 +1,9 @@
 package Factory.Models;
 
-import Enum.*;
+import Enum.Rooms;
 import Factory.Interfaces.SmartHome;
 import Models.SmartObject.Heater;
+import Models.HelpChecks;
 import Models.SmartObject.SmartObject;
 
 import java.util.Scanner;
@@ -14,8 +15,9 @@ public class SmartHomeHeater implements SmartHome {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите id:     ");
         int idSmartObject = input.nextInt();
-        System.out.println("Подключено ли устройство к сети? true/false:    ");
-        boolean isActive = input.nextBoolean();
+        System.out.println("Подключено ли устройство к сети? 0/1:    ");
+        int cur = HelpChecks.spellCheck(0, 1);
+        boolean isActive = cur == 1;
 //        System.out.println("Введите местоположение умного устройства:");
         input.nextLine();
         System.out.println("Введите краткое описание устройства:    ");
@@ -25,4 +27,3 @@ public class SmartHomeHeater implements SmartHome {
         return new Heater(idSmartObject, isActive, pos, objectDescription, temperature);
     }
 }
-
