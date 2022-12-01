f699b35bc991b7c5195b680b74fa4303c5e9fe29
Zazulin Nikita <zazulin.nik@mail.ru>
2022-12-02 00:33:17 +0300

    Add lad5_3 of Nikita Zazulin

---
+++ nikita.zazulin/lab_5_3/src/studentClass/StudentArrayList.java
@@ -0,0 +1,103 @@
+package studentClass;
+
+import classHelper.Months;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StudentArrayList {
+
+    private static final int SIZE = 1200;
+
+    private final List<Student> students;
+
+    public StudentArrayList() {
+
+        this.students = new ArrayList<>();
+
+    }
+
+    public void fillRandom() {
+
+        long start = System.nanoTime();
+
+        IntStream.range(0, SIZE)
+                .mapToObj(i -> Student.createRandomStudent())
+                .forEach(students::add);
+
+        long finish = System.nanoTime();
+        long elapsed = finish - start;
+
+        System.out.println("Time ms: " + elapsed);
+
+    }
+
+    public void fillFromConsole() {
+
+        IntStream.range(0, SIZE)
+                .mapToObj(i -> Student.readStudentFromConsole())
+                .forEach(students::add);
+
+    }
+
+    public void Clear() {
+
+        this.students.clear();
+
+    }
+
+    public int countMale(Months month) {
+
+        return (int) students.stream().
+                filter(student -> (month.toStr().equals(student.getMonth())) && !student.isSex())
+                .count();
+
+    }
+    public int countFemale(Months month) {
+
+        return (int) students.stream().
+                filter(student -> (month.toStr().equals(student.getMonth())) && student.isSex())
+                .count();
+
+    }
+
+    public void findCount() {
+
+        long start = System.nanoTime();
+        Arrays.stream(Months.values()).forEach(month -> {
+
+            System.out.println(month + ": ");
+            System.out.println("Male: " + countMale(month) + "\n" + "Female: " + countFemale(month));
+
+        });
+
+        long finish = System.nanoTime();
+        long elapsed = finish - start;
+
+        System.out.println("Time ms: " + elapsed);
+
+    }
+
+    public void findBiggerFemalesThanMales() {
+
+        long start = System.nanoTime();
+
+        System.out.println("Months where female students bigger then male students: ");
+
+        Arrays.stream(Months.values())
+                .filter(month -> countFemale(month) > countMale(month))
+                .forEach(System.out::println);
+
+        long finish = System.nanoTime();
+        long elapsed = finish - start;
+
+        System.out.println("Time ms: " + elapsed);
+
+    }
+
+}
+
+
+
