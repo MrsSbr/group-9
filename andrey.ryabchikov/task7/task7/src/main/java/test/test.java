a6b6a0308387dc70527fc97457e1c52e118a40f1
andreyRyabchikov <45429367+andreyRyabchikov@users.noreply.github.com>
2022-12-13 12:50:08 +0300

    task7

---
+++ andrey.ryabchikov/task7/task7/src/main/java/test/test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class test implements Service {
+
+
+    public void counter() {
+        int a = 0;
+        while (a < 10000) {
+
+            a++;
+
+        }
+
+    }
+
+
+}
