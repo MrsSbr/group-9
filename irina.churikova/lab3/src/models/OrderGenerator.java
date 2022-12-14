4579aa110f31029bd884dde4242e78170d313468
IraChurikova <104374839+IraChurikova@users.noreply.github.com>
2022-12-14 09:23:35 +0300

    Edits

--- irina.churikova/lab3/src/models/OrderGenerator.java
+++ irina.churikova/lab3/src/models/OrderGenerator.java
@@ -6,22 +6,25 @@ import java.util.List;
 import java.util.Random;
 
 public class OrderGenerator {
-    private static final List<String>pizzaNames = new ArrayList<>(Arrays.asList("Маргарита", "Пепперони", "Итальянская", "Европейская",
+    private static final List<String> pizzaNames = new ArrayList<>(Arrays.asList("Маргарита", "Пепперони", "Итальянская", "Европейская",
             "4 сыра", "Морская", "Цезарь", "Карбонара", "Закрытая пицца", "Веган пицца"));
-    private static final List<String>ingredientOfMargarita = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+    private static final List<String> ingredientOfMargarita = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Бекон", "Сливки", "Чеснок", "Зелень"));
-    private static final List<String>ingredientOfPepperoni = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+    private static final List<String> ingredientOfPepperoni = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Салями", "Базилик"));
-    private static final List<String>ingredientOfItalia = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+    private static final List<String> ingredientOfItalia = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Грибы", "Курица"));
-    private static final List<String>ingredientOfEuropa = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
-            "Перец", "Ветчина", "Колбаса","Петрушка"));
-    private static final List<String>ingredientOfFourCheese = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+    private static final List<String> ingredientOfEuropa = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+            "Перец", "Ветчина", "Колбаса", "Петрушка"));
+    private static final List<String> ingredientOfFourCheese = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Cыр Моцарелла", "Сыр Маасдам", "Сыр Пармезан"));
-    private static final List<String>ingredientOfSea = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
-            "Кальмары", "Мидии", "Креветки","Кукуруза"));
-    private static final List<String>ingredientOfCarbonara = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+    private static final List<String> ingredientOfSea = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+            "Кальмары", "Мидии", "Креветки", "Кукуруза"));
+    private static final List<String> ingredientOfCarbonara = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Перец", "Cметана", "Бекон"));
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
     private static final List<String>ingredientOfCaesar = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
             "Салат", "Куриная грудка", "Майонез","Сухарики"));
     private static final List<String>ingredientOfClosedPizza = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
@@ -30,15 +33,42 @@ public class OrderGenerator {
             "Грибы", "Специи","Перец"));
     private static final List<List<String>> ingredients = new ArrayList(Arrays.asList(ingredientOfMargarita,ingredientOfPepperoni,ingredientOfItalia,ingredientOfEuropa,ingredientOfFourCheese,
             ingredientOfSea,ingredientOfCarbonara, ingredientOfCaesar,ingredientOfClosedPizza,ingredientOfVegetarians ) );
+    private static final List<Double> prices = new ArrayList<>(Arrays.asList(300.0,50.0,150.5,50.0,830.0,750.0,100.7,550.9,230.0,800.0));
+    private static String randName( int nameIndex) {
+        return pizzaNames.get(nameIndex);
     }
     private static double randPrice( int nameIndex) {
+=======
+=======
+>>>>>>> 4909c2c4114c79e98e87c0bc8ecc776740f63c4a
+=======
+>>>>>>> 4909c2c4114c79e98e87c0bc8ecc776740f63c4a
+    private static final List<String> ingredientOfCaesar = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+            "Салат", "Куриная грудка", "Майонез", "Сухарики"));
+    private static final List<String> ingredientOfClosedPizza = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+            "Кабачки", "Каперсы", "Мука", "Дрожжи"));
+    private static final List<String> ingredientOfVegetarians = new ArrayList<>(Arrays.asList("Лук", "Томаты", "Сыр",
+            "Грибы", "Специи", "Перец"));
+    private static final List<List<String>> ingredients = new ArrayList(Arrays.asList(ingredientOfMargarita, ingredientOfPepperoni, ingredientOfItalia, ingredientOfEuropa, ingredientOfFourCheese,
+            ingredientOfSea, ingredientOfCarbonara, ingredientOfCaesar, ingredientOfClosedPizza, ingredientOfVegetarians));
+
+    private static double randPrice(int nameIndex) {
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 4909c2c4114c79e98e87c0bc8ecc776740f63c4a
+=======
+>>>>>>> 4909c2c4114c79e98e87c0bc8ecc776740f63c4a
+=======
+>>>>>>> 4909c2c4114c79e98e87c0bc8ecc776740f63c4a
         return prices.get(nameIndex);
     }
+
     private static List<String> randIngredients(int index) {
         return ingredients.get(index);
     }
+
     public static Order randCase() {
-         Random random = new Random();
+        Random random = new Random();
         int nameIndex = random.nextInt(0, pizzaNames.size());
         return new Order(randName(nameIndex), randPrice(nameIndex), randIngredients(nameIndex));
     }
